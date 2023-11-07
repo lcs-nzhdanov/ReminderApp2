b6287060fb8e337b93b6a9b7e456e6692f382f7f
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2023-11-06 19:12:32 -0500

    Initial Commit

---
+++ ReminderApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReminderApp2
+//
+//  Created by Nikita Zhdanov on 2023-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReminderApp2/ReminderApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp2App.swift
+//  ReminderApp2
+//
+//  Created by Nikita Zhdanov on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
